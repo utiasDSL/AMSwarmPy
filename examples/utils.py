a5af0b0484f5ce620c09472fe2492b0692f27373
Martin Schuck <martin.schuck@tum.de>
2025-07-14 12:09:24 +0200

    Remove old code

--- examples/utils.py
+++ examples/utils.py
@@ -2,77 +2,7 @@ import mujoco
 import numpy as np
 from crazyflow import Sim
 from numpy.typing import NDArray
-from scipy.spatial.transform import Rotation as R
-
-
-def generate_time_stamps(num_waypoints, min_interval=1.0, max_interval=4):
-    """Generate a sorted array of time stamps for waypoint creation.
-
-    Note: If num_waypoints is 1 or less, the function returns an array containing only the initial
-        time stamp 0.
-
-    Returns:
-        An array of time stamps, starting from 0, sorted in ascending order.
-    """
-    if num_waypoints <= 1:
-        return np.array([0])
-
-    intervals = np.random.uniform(min_interval, max_interval, size=num_waypoints - 1)
-    time_stamps = np.insert(np.cumsum(intervals), 0, 0)
-    return time_stamps
-
-
-def generate_random_positions(
-    n_drones, min_distance, max_change, scaling=1.0, previous_positions=None
-):
-    """Generate random positions for drones that are at least min_distance from each other."""
-    if previous_positions is None:
-        pos = (np.random.rand(n_drones, 3) - 0.5) * 2 * scaling
-        pos[..., 2] += scaling
-    else:
-        pos = previous_positions + (np.random.rand(n_drones, 3) - 0.5) * 2 * max_change
-    pos[..., 2] = np.clip(pos[..., 2], 0.1, 10)
-
-    # Resample positions that are too close until all are at least min_distance apart
-    max_attempts = 1000
-    for _ in range(max_attempts):
-        # Compute pairwise distances
-        dists = np.linalg.norm(pos[:, np.newaxis, :] - pos[np.newaxis, :, :], axis=-1)
-        # Set diagonal to a large value to ignore self-distance
-        np.fill_diagonal(dists, np.inf)
-        # Find pairs that are too close
-        too_close = np.where(dists < min_distance)
-        if len(too_close[0]) == 0:
-            break  # All positions are valid
-        # Resample positions for drones that are too close to any other
-        to_resample = set(too_close[0]) | set(too_close[1])
-        for idx in to_resample:
-            if previous_positions is None:
-                pos[idx] = (np.random.rand(3) - 0.5) * 2 * scaling
-            else:
-                pos[idx] = previous_positions[idx] + (np.random.rand(3) - 0.5) * 2 * max_change
-            pos[..., 2] = np.clip(pos[..., 2], 0.1, 10)
-    else:
-        raise RuntimeError(
-            "Could not generate positions with required min_distance after many attempts."
-        )
-    return pos
-
-
-def generate_random_waypoints(n_drones, num_waypoints, min_distance=0.5, duration=10.0):
-    time_stamps = np.tile(np.linspace(0, duration, num_waypoints), (n_drones, 1)).T  # [T, n_drones]
-    scaling = 2.0 * n_drones ** (1 / 3)
-    pos = np.zeros((num_waypoints, n_drones, 3))
-    vel = np.zeros_like(pos)
-    acc = np.zeros_like(pos)
-    # Initial positions
-    pos[0] = (np.random.rand(n_drones, 3) - 0.5) * 2 * scaling
-    pos[0, :, 2] += scaling
-    for i in range(1, num_waypoints):
-        # Random walk for each drone, ensure z stays positive
-        pos[i] = pos[i - 1] + (np.random.rand(n_drones, 3) - 0.5) * min_distance
-        pos[i, :, 2] = np.clip(pos[i, :, 2], 0.1, 10)
-    return {"time": time_stamps, "pos": pos, "vel": vel, "acc": acc}
+from scipy.spatial.transform import Rotation as R  # noqa: N817
 
 
 def draw_line(
